8be7b73ca873dff3ab5b96b3902b80ceb0aca930
Angel Rosmend <60056902+angelrosmend@users.noreply.github.com>
2020-10-17 23:48:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './contexts/AuthContext';
 
-
 function App() {
   return (
     <div className="App">
